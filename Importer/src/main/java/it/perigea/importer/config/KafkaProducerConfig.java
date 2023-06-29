83e72bf0629b3ac637849b0a0e63ea46f05a9518
DavideSchipilliti <131289972+DavideSchipilliti@users.noreply.github.com>
2023-06-29 11:36:06 +0200

    refactoring kafka config
    config moved in application properties

--- Importer/src/main/java/it/perigea/importer/config/KafkaProducerConfig.java
+++ Importer/src/main/java/it/perigea/importer/config/KafkaProducerConfig.java
@@ -1,41 +1,23 @@
 package it.perigea.importer.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+
+/* 
+ * Questa classe serve a configurare kafka, viene creata un'istanza di ProducerFactory<String, String> con le configurazioni impostate
+ * nell'application properties. In questo modo possiamo usarla per creare il bean kafkaTemplate sul quale verrà chiamato il metodo send.
+ * */
 
 @EnableKafka
 @Configuration
 public class KafkaProducerConfig {
-	
-	@Bean
-    ProducerFactory<String, String> responseProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-          ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, 
-          "localhost:9092");
-        configProps.put(
-          ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, 	//Modifico il serializer da usare, in questo modo al posto di serializzare in stringhe uso json
-          StringSerializer.class);
-        configProps.put(
-          ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, //Sia per la chiave che per il value.
-          JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
 
     @Bean
-    KafkaTemplate<String, String> responseKafkaTemplate() {
-        return new KafkaTemplate<>(responseProducerFactory());
+    KafkaTemplate<String, String> responseKafkaTemplate(ProducerFactory<String, String> responseProducerFactory) {
+        return new KafkaTemplate<>(responseProducerFactory);
     }
-    
     
 }
